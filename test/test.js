8cb22a4ce68364311942d02fc76bc663799f3e6c
Andrey Sitnik <andrey@sitnik.ru>
2015-03-29 15:05:23 +0300

    Better syntax

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var expect  = require('chai').expect;
 var plugin = require('../');
 
 var test = function (input, output, opts) {
-    expect(postcss(plugin(opts)).process(input).css).to.eql(output);
+    expect(postcss([plugin(opts)]).process(input).css).to.eql(output);
 };
 
 describe('PLUGIN_NAME', function () {
