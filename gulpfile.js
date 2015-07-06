07f201624aec0e0e76b2cdd425c76a86decb33bb
Jan Wirth <jan_wirth@visual4.de>
2015-07-06 14:00:00 +0200

    Improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+
+files = ['index.js', 'test/*.js', 'gulpfile.js'];
 
 gulp.task('lint', function () {
     var eslint = require('gulp-eslint');
-    return gulp.src(['index.js', 'test/*.js', 'gulpfile.js'])
+    return gulp.src(files)
+        .pipe(plumber())
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -10,7 +14,13 @@ gulp.task('lint', function () {
 
 gulp.task('test', function () {
     var mocha = require('gulp-mocha');
-    return gulp.src('test/*.js', { read: false }).pipe(mocha());
+    return gulp.src('test/*.js', { read: false })
+        .pipe(plumber())
+        .pipe(mocha());
 });
 
 gulp.task('default', ['lint', 'test']);
+
+gulp.task('watch', function() {
+    gulp.watch(files, ['lint', 'test']);
+});
