952b8757d34c007ddc3f0f2bd4991785cbb4732a
Bogdan Chadkin <trysound@yandex.ru>
2015-08-12 15:15:33 +0300

    Fix error reporting

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp');
 
 var files = ['index.js', 'test/*.js', 'gulpfile.js'];
 
-gulp.task('lint', function (done) {
+gulp.task('lint', function () {
     var eslint = require('gulp-eslint');
     return gulp.src(files)
         .pipe(eslint())
         .pipe(eslint.format())
-        .pipe(eslint.failAfterError()).on('error', done);
+        .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function (done) {
+gulp.task('test', function () {
     var mocha = require('gulp-mocha');
     return gulp.src('test/*.js', { read: false })
-        .pipe(mocha()).on('error', done);
+        .pipe(mocha());
 });
 
 gulp.task('default', ['lint', 'test']);
