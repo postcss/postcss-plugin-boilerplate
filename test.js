bebd985b7696cabc2648ca4de0fdd7fe0b71648b
Andrey Sitnik <andrey@sitnik.ru>
2015-11-12 15:44:37 +0300

    Use ava instead of mocha and chai

---
+++ test.js
@@ -0,0 +1,20 @@
+import postcss from 'postcss';
+import test    from 'ava';
+
+import plugin from './';
+
+function run(t, input, output, opts = { }) {
+    return postcss([ plugin(opts) ]).process(input)
+        .then( result => {
+            t.same(result.css, output);
+            t.same(result.warnings().length, 0);
+        });
+}
+
+/* Write tests here
+
+test('does something', t => {
+    return run(t, 'a{ }', 'a{ }', { });
+});
+
+*/
