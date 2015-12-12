c5e12fa9ec74fd133689d49933fea74f948f7334
Bogdan Chadkin <trysound@yandex.ru>
2015-12-12 15:24:02 +0300

    Use 'is' method to compare simple types

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ import plugin from './';
 function run(t, input, output, opts = { }) {
     return postcss([ plugin(opts) ]).process(input)
         .then( result => {
-            t.same(result.css, output);
-            t.same(result.warnings().length, 0);
+            t.is(result.css, output);
+            t.is(result.warnings().length, 0);
         });
 }
 
