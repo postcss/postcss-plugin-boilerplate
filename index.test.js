bf9575993aec18b2b84e020d9071d06b37c12eb2
Andrey Sitnik <andrey@sitnik.ru>
2016-11-24 04:59:53 +0300

    Replace AVA to Jest

---
+++ index.test.js
@@ -0,0 +1,19 @@
+const postcss = require('postcss');
+
+const plugin = require('./');
+
+function run(input, output, opts = { }) {
+    return postcss([ plugin(opts) ]).process(input)
+        .then( result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
+}
+
+/* Write tests here
+
+it('does something', () => {
+    return run('a{ }', 'a{ }', { });
+});
+
+*/
