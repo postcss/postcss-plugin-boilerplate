85f8b49ac3db0d86038b87068abeb3ad5a843b36
Andrey Sitnik <andrey@sitnik.ru>
2016-12-24 17:44:48 +1000

    Fix tests for node.js 4

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-const postcss = require('postcss');
+var postcss = require('postcss');
 
-const plugin = require('./');
+var plugin = require('./');
 
-function run(input, output, opts = { }) {
+function run(input, output, opts) {
     return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
+        .then(result => {
             expect(result.css).toEqual(output);
             expect(result.warnings().length).toBe(0);
         });
